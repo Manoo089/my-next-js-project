0795b721b0c6783fca696cd6a2ab348c6b80b18a
Manuel Hohenadl <hm@campudus.com>
2022-07-07 17:57:56 +0200

    feat: productcard preparation what is needed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import ProductCard from "./Components/ProductCard/ProductCard";
+
 export default function Home() {
   return (
     <>
       <h1>Hier entsteht ein Online-Shop</h1>
+      <ProductCard />
     </>
   )
 };
