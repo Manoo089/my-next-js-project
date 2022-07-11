cf0a0155b09ba5bae7f30e2ba67bd06e3b30ad73
Manuel Hohenadl <hm@campudus.com>
2022-07-11 21:14:45 +0200

    feat: add Button component to storybook

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,9 +1,25 @@
+import PropTypes from "prop-types";
 import React from "react";
 
-export default function Button() {
+const Button = ({ disabled = false, label, onClick }) => {
+  const handleOnClick = () => {
+    if (!disabled) {
+      console.log("clicked");
+    }
+  };
+
+  const isDisabled = disabled ? "Button--disabled" : null;
+
   return (
-    <button className="Button" type="button">
-      Content
+    <button className={["Button", [`${isDisabled}`]].join(" ")} onClick={onClick}>
+      {label}
     </button>
   );
 };
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  label: PropTypes.string,
+};
+
+export default Button;
--- components/Button/Button.stories.js
+++ components/Button/Button.stories.js
@@ -3,9 +3,20 @@ import React from "react";
 import Button from "./Button";
 
 export default {
-    title: "Components/Button",
-    component: Button,
-    excludeStories: ['Template'],
+  title: "Components/Button",
+  component: Button,
+  excludeStories: ["Template"],
 };
 
-export const Template = () => <Button />
+export const Template = args => <Button {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {
+  label: "Content",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  label: "Content",
+};
