2f5df3e98da5102c00e310f4653c9f2d720b8910
Manuel Hohenadl <hm@campudus.com>
2022-07-13 16:55:35 +0200

    feat: add layout and head

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import Head from "next/head";
+
 import ProductCard from "../components/ProductCard/ProductCard";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Online-Shop</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1>Hier entsteht ein Online-Shop</h1>
       <ProductCard />
     </>
-  )
+  );
 };
