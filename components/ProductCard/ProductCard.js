7b50e5cd071bd93c924eda2ae93230da322b3af7
Manuel Hohenadl <hm@campudus.com>
2022-07-14 16:12:53 +0200

    fix: add new jsondata

--- components/ProductCard/ProductCard.js
+++ components/ProductCard/ProductCard.js
@@ -1,41 +1,43 @@
 import React from "react";
-
+import { jsondb } from "../../jsondb/products";
 import ProductCardOptions from "./ProductCardOptions";
 
-const data = [
-  {
-    id: 1,
-    category: "Shoes",
-    manufacturer: "Asics",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci facere fugit quod illum eligendi veritatis autem saepe! Deleniti cumque possimus libero architecto debitis...",
-    specialOffer: "Free Shipping",
-    image: "blue_sport_shoes.jpg",
-    alt: "Sport Shoes",
-    price: "$65 USD",
-    sizes: ["40", "41", "42"],
-    quantity: 8,
-    colors: ["blue", "red", "black"],
-  },
-];
+
+// const data = [
+//   {
+//     id: 1,
+//     category: "Shoes",
+//     manufacturer: "Asics",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci facere fugit quod illum eligendi veritatis autem saepe! Deleniti cumque possimus libero architecto debitis...",
+//     specialOffer: "Free Shipping",
+//     image: "blue_sport_shoes.jpg",
+//     alt: "Sport Shoes",
+//     price: "$65 USD",
+//     sizes: ["40", "41", "42"],
+//     quantity: 8,
+//     colors: ["blue", "red", "black"],
+//   },
+// ];
 
 export default function ProductCard() {
+   const data = jsondb.products;
   return (
     <>
       {data.map(product => {
         return (
-          <article className="ProductCard" key={product.id}>
-            <aside className="ProductCard__info">{product.specialOffer}</aside>
+          <article className="ProductCard" key={product.name}>
+            <aside className="ProductCard__info"></aside>
             <header className="ProductCard__header">
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img className="ProductCard__image" src={`/${product.image}`} alt={product.alt} />
+              <img className="ProductCard__image" src={`/${product.image}`} alt={product.name} />
             </header>
             <main className="ProductCard__main">
-              <h2>{product.manufacturer}</h2>
+              <h2>{product.name}</h2>
               <p className="ProductCard__main--description">{product.description}</p>
             </main>
             <footer className="ProductCard__footer">
-              <ProductCardOptions value={product.colors} price={product.price} productSize={product.sizes} productQuantity={product.quantity} />
+              {/* <ProductCardOptions value={product.colors} price={product.price} productSize={product.sizes} productQuantity={product.quantity} /> */}
             </footer>
           </article>
         );
---
+++ jsondb/products.js
@@ -0,0 +1,77 @@
+const jsondb = {
+    procucts: [
+        {
+          name: 'Crispy Burger',
+          description: 'American Style Burger',
+          category: 'Hauptgericht',
+          price: 6.99,
+          url: 'burger',
+          image: '/images/products/burger.jpg'
+        },
+         {
+          name: 'Coca Cola',
+          description: 'Eisgekühlte Cola',
+          category: 'Trinken',
+          price: 1.99,
+          url: 'cola',
+          image: '/images/products/cola.jpg'
+        },   
+        {
+          name: 'Erdbeer Eis',
+          beschreibung: 'Eis mit Erdbeeren und Sahne',
+          category: 'Nachspeise',
+          price: 2.99,
+          url: 'erdbeereis',
+          image: '/images/products/eis.jpg'
+        },
+         {
+          name: 'Falaffel',
+          description: 'Orientalische Falaffel',
+          category: 'Hauptgericht',
+          price: 6.99,
+          url: 'falaffel',
+          image: '/images/products/falaffel.jpg'
+        },
+         {
+          name: 'Lahmacun',
+          description: 'Turkish Style Lahmacun',
+          category: 'Hauptgericht',
+          price: 4.50,
+          url: 'lahmacun',
+          image: '/images/products/lahmacun.jpg'
+        },
+         {
+          name: 'Lasagne',
+          description: 'Lasagne aus Italien',
+          category: 'Hauptgericht',
+          price: 8.50,
+          url: 'lasagne',
+          image: '/images/products/lasagne.jpg'
+        },  
+         {
+          name: 'Schokoladen Muffin',
+          description: 'Sehr süßer Sckoko Muffin',
+          category: 'Nachspeise',
+          price: 3.20,
+          url: 'muffin',
+          image: '/images/products/muffin.jpg'
+        },  
+         {
+          name: 'Pizza Original',
+          description: '4 seasons Pizza',
+          category: 'Hauptgericht',
+          price: 7.50,
+          url: 'pizza',
+          image: '/images/products/pizza.jpg'
+        }, 
+         {
+          name: 'Süßkartoffel Pommes',
+          description: 'Süßkartoffel Pommes mit Dip',
+          category: 'Hauptgericht',
+          price: 4.80,
+          url: 'pommmes',
+          image: '/images/products/pommes.jpg'
+        },                                           
+    ]
+   }
+   export default jsondb;
