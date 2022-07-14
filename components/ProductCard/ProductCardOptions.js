426a411b50ab8168abf24a501389dc746b8c3d62
Manuel Hohenadl <hm@campudus.com>
2022-07-14 17:51:27 +0200

    refactor: a first refactor

--- components/ProductCard/ProductCardOptions.js
+++ components/ProductCard/ProductCardOptions.js
@@ -22,7 +22,7 @@ export default function ProductCardOptions({ price, productSize, productQuantity
   return (
     <article className="ProductCardOptions">
       <header className="ProductCardOptions__price">
-        <h3>{price}</h3>
+        <h3>{price} €</h3>
       </header>
 
       <main className="ProductCardOptions__selects">
@@ -33,16 +33,11 @@ export default function ProductCardOptions({ price, productSize, productQuantity
           productQuantity={quantityCount(productQuantity)}
           onChange={handleOnChange}
         />
-
-        <div className="ProductCardOptions__colors">
-          <label htmlFor="colors">Colors:</label>
-
-          {value.map(v => {
-            return <RadioGroupColor key={v} name="colors" value={v.toString()} />;
-          })}
-        </div>
       </main>
-      <Button label="Add to Cart" onClick={() => console.log("ProductCard Button wurde geklickt!")} />
+      
+      <footer>
+        <Button label="Add to Cart" onClick={() => console.log("ProductCard Button wurde geklickt!")} />
+      </footer>
     </article>
   );
-};
+}
---
+++ pages/vorspeisen.js
@@ -0,0 +1,7 @@
+export default function Vorspeisen() {
+    return (
+        <div>
+            <h2>Vorspeisen</h2>
+        </div>
+    )
+}
